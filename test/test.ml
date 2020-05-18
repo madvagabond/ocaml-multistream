9f0759934ca559e8421f9d25994b6924354ffc0d
Sam Riyad <flatmapds@gmail.com>
2020-05-18 02:05:29 -0400

    refactored tests to use multistream-unix

--- test/test.ml
+++ test/test.ml
@@ -1,59 +1,10 @@
-module IO = struct
-
-
-  open Lwt.Infix
-         
-         
-  module C = Mirage_channel.Make(Mirage_flow_unix.Fd)
-
-  
-  type error = [
-      `Read_error of C.error | `Eof | `Write_error of C.write_error
-  ]
-
-  
-  type t = C.t
 
 
 
-  let eof_to_err = function 
-    | Ok `Data buf -> Ok buf
-    | Ok `Eof -> Error `Eof
-    | Error e -> Error (`Read_error e)
-  
- 
-  let read_some t len =
-    C.read_some t ~len >|= eof_to_err
 
-  
-  let read_exact t len =
-    C.read_exactly t ~len >|= fun x ->
-    let y = eof_to_err x in
-    Result.map Cstruct.concat y
+module MS = Multistream_unix
 
-
-  let write t buf =
-    let _ = C.write_buffer t buf in
-
-    Lwt_result.map_err
-      (fun e -> `Write_error e)
-      (C.flush t) 
-
-
-  let close t =
-    C.close t >|= fun _ -> ()
-
-  let read t = C.read_some t ~len:1024 >|= eof_to_err
-
-  let create = C.create
-
-  
-end
-
-
-
-module MS = Multistream.Make(IO)
-
+module IO = MS.IO
 
 
 let create_conn () =
@@ -80,6 +31,7 @@ let print_error =
   | Error `Codec_error -> failwith "codec error"
   | Error `Protocol_mismatch -> failwith "protocol mismatch"
   | Error `Na -> failwith "na"
+
 
 
 
